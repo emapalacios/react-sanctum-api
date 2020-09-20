900cc75ab5964e43d416222d478b5421e5927ec1
Emanuel <manupalacios19@gmail.com>
2020-09-20 19:18:20 -0300

    FIX v8 routing

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books', 'BookController@create');
+Route::get('/books', 'App\Http\Controllers\BookController@index');
