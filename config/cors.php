31eda7e2e3a50fbfae2267709c33e446b6a90f4d
Emanuel <manupalacios19@gmail.com>
2021-05-08 19:24:25 -0300

    sanctum config

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', '/login'],
+    'paths' => ['api/*', '/login', 'logout', 'sanctum/csrf-cookie', '/books'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://localhost:3000,http://localhost:3001')),
 
     'allowed_headers' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
